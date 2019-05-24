433ea9af5b0924111f6e0c4e8438a6542d1514b1
p66c206p <42195846+p66c206p@users.noreply.github.com>
2019-05-24 23:07:43 +0900

    int * intをlongに入れようとしても入るのはint

---
+++ ARC091D.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        long count = 0;
+        
+        if (k == 0) {
+            count = (long)n * n;
+        } else {
+            for (int i = k + 1; i <= n; i++) {
+                count += (long)(n / i) * (i - k);
+                if (n % i >= k) {
+                    count += (long)n % i - (k - 1);
+                }
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
