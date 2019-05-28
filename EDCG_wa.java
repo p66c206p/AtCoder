a659278120e915094a2ec26658aefe1f8d5ed3fe
p66c206p <42195846+p66c206p@users.noreply.github.com>
2019-05-28 18:12:19 +0900

    classの書き方、再帰関数

---
+++ EDCG_wa.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            points[i] = new Point(i);
+        }
+        
+        while (m-- > 0) {
+            int parent = sc.nextInt() - 1;
+            int child = sc.nextInt() - 1;
+            points[parent].addChild(points[child]);
+        }
+        
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int length = getLength(0, points[i]);
+            max = Math.max(max, length);
+        }
+        
+        System.out.println(max);
+    }
+    
+    public static class Point {
+        int name;
+        List<Point> children;
+        
+        Point(int name) {
+            this.name = name;
+            this.children = new ArrayList<Point>();
+        }
+        
+        void addChild(Point p) {
+            this.children.add(p);
+        }
+    }
+    
+    public static int getLength(int depth, Point p) {
+        int length = 0;
+        
+        if (p.children.isEmpty()) {
+            length = depth;
+        } else {
+            for (Point child : p.children) {
+                int l = getLength(depth + 1, child);
+                length = Math.max(length, l);
+            }
+        }
+        
+        return length;
+    }
+}
