a9bd14acf7f0dfca03f9c8e2a8cc2b51dbc08097
p66c206p <42195846+p66c206p@users.noreply.github.com>
2019-05-29 18:28:19 +0900

    Create 0classの書き方.java

---
+++ 0classの書き方.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    
+    // ここに書くなら(public) static class
+    public static class Hoge {
+    
+    }
+}
+
+// ここに書くならclass ※public, static不要
+class Hoge {
+}
