6818942ae36250908e895e61ef802f1bf507928b
p66c206p <42195846+p66c206p@users.noreply.github.com>
2019-06-05 19:12:47 +0900

    Stringはchar[]で 配列のscanfは&いらない

---
+++ C/ABC126A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    // Your code here!
+    int length, index;
+    scanf("%d%d", &length, &index);
+    
+    // String型はない。char[]で受け取る。
+    char s[length];
+    scanf("%s", s); // 配列なので、&sでなくs
+    
+    // 小文字にする
+    s[index - 1] += 32;
+    
+    printf("%s\n", s);
+}
