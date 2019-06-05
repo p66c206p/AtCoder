eb738c11fee2c92a4db317058ffed97e06013ae4
p66c206p <42195846+p66c206p@users.noreply.github.com>
2019-06-05 18:47:24 +0900

    Create ABC127A.c

---
+++ C/ABC127A.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+    // Your code here!
+    int age, price;
+    scanf("%d%d", &age, &price);
+    
+    if (age >= 13) {
+        printf("%d", price);
+    } else if (age <= 5) {
+        printf("0");
+    } else {
+        printf("%d", price / 2);
+    }
+}
