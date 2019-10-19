004feda23c989c0e1b72373461002089d1d6949d
p66c206p <42195846+p66c206p@users.noreply.github.com>
2019-10-19 23:54:11 +0900

    しゃくとり法 条件を満たす区間の数を返す

--- 0shakutori.java
+++ 0shakutori.java
@@ -25,3 +25,34 @@ public static long shakutori(int[] array, long k) {
     
     return count;
 }
+
+public static long shakutori(int[] array, long k) {
+    // 条件を満たす区間の数を返す
+    // この場合の条件: 任意の区間の和がk未満になる
+    
+    long count = 0;
+    int n = array.length;
+    int right = 0;
+    
+    long sum = 0;
+    boolean canGoNext = right < n && sum + array[right] < k;
+    
+    for (int left = 0; left < n; left++) {
+        while (canGoNext) {
+            sum += array[right];
+            right++;
+            
+            canGoNext = right < n && sum + array[right] < k;
+        }
+        
+        int countGot = right - left;
+        if (countGot) {
+            count += countGot;
+            sum -= array[left];
+        } else {
+            right = left + 1;
+        }
+    }
+    
+    return count;
+}
