c9b48b310c612e90ab9cfc6f7691d1551ed689a8
p66c206p <42195846+p66c206p@users.noreply.github.com>
2019-11-03 23:34:16 +0900

    prime(n) nの素因数の個数

---
+++ ABC142D.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        
+        long gcd = 6;
+        
+        System.out.println(prime(gcd));
+    }
+    
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        
+        return gcd(b, a % b);
+    }
+    
+	public static long prime(long n) {
+	    // nの素因数の個数(1を含む)
+	    
+		int count = 1;
+		
+		long d = n;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (d % i == 0) {
+				count++;
+				while (d % i == 0) {
+					d = d / i;
+				}
+			}
+		}
+		
+		if (d != 1) {
+			count++;
+		}
+		
+		return count;
+	}
+}
