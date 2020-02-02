76b8493cf8f1cbe52233564ca1d54d04e4dba716
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-02-02 16:40:15 +0900

    nCk 組み合わせ

---
+++ 0nCkMOD.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    static int MAX = 510000;
+    static int MOD = 1000000007;
+    static long[] fac;
+    static long[] finv;
+    static long[] inv;
+    
+    public static void main(String[] args) throws Exception {
+
+    }
+    
+    // テーブルを作る前処理
+    public static void COMinit() {        
+        fac = new long[MAX];
+        finv = new long[MAX];
+        inv = new long[MAX];
+        
+        fac[0] = fac[1] = 1;
+        finv[0] = finv[1] = 1;
+        inv[1] = 1;
+        for (int i = 2; i < MAX; i++){
+            fac[i] = fac[i - 1] * i % MOD;
+            inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+            finv[i] = finv[i - 1] * inv[i] % MOD;
+        }
+    }
+    // 二項係数計算
+    public static long nCkMOD(int n, int k){
+        if (n < k) return 0;
+        if (n < 0 || k < 0) return 0;
+        return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+    }
+}
