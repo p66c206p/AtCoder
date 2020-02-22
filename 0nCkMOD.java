d9e9328f80a60e174b144f8794f64c1af35ed867
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-02-22 22:43:17 +0900

    nCk 組み合わせ(1 ≦ k ≦ n ≦ 10^7 程度)

--- 0nCkMOD.java
+++ 0nCkMOD.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Main {
     static int MAX = 10010017;  // (1 ≦ k ≦ n ≦ 10^7 程度)
     static int MOD = 1000000007;
-    static long[] fac;
+    static long[] fac;  // 階乗
     static long[] finv;
-    static long[] inv;
+    static long[] inv;  // 逆元
     
     public static void main(String[] args) throws Exception {
         // Your code here!
@@ -13,7 +13,7 @@ public class Main {
         
         COMinit();
         
-        System.out.println(nCkMOD(10000000, 500000));
+        System.out.println(nCkMOD(10000000, 10000000));
     }
     
     // テーブルを作る前処理
@@ -37,5 +37,16 @@ public class Main {
         if (n < k) return 0;
         if (n < 0 || k < 0) return 0;
         return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+    }
+    
+    public static long modpow(long a, long n) {
+        long res = 1;
+        while (n > 0) {
+            long tmp = n & 1;
+            if (tmp > 0) res = res * a % MOD;
+            a = a * a % MOD;
+            n >>= 1;
+        }
+        return res;
     }
 }
