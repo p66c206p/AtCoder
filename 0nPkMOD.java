f3c46edd5e3b69f4f3be4d5ee9a82279273eadfe
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-03-07 20:23:48 +0900

    nPk 順列

--- 0nPkMOD.java
+++ 0nPkMOD.java
@@ -1,6 +1,6 @@
 public static long nPkMOD(long n, long k, long mod) {
     long res = 1;
-    for (long i = n; i > k; i--) {
+    for (long i = n; i > n - k; i--) {
         res = res * i % mod;
     }
 
