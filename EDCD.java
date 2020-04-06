d5b09e670994552f51528a0458bd413c37dc97d5
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-04-06 22:29:41 +0900

    ナップサック問題

--- EDCD.java
+++ EDCD.java
@@ -6,23 +6,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int W = sc.nextInt();
-        int[] weights = new int[n];
-        long[] values = new long[n];
+        int[] weight = new int[n];
+        int[] value = new int[n];
         for (int i = 0; i < n; i++) {
-            weights[i] = sc.nextInt();
-            values[i] = sc.nextLong();
+            weight[i] = sc.nextInt();
+            value[i] = sc.nextInt();
         }
         
-        long[][] dp = new long[n + 1][W + 1];
+        // dp[i][w]: 品物0~iまで考慮した上で、重量wでの最高価値
+        int[][] dp = new int[n + 1][W + 1];
         for (int w = 0; w <= W; w++) {
             dp[0][w] = 0;
         }
         
+        // 品物0, (0,1), (0,1,2), ... だけを考えた時を順々にやる
         for (int i = 0; i < n; i++) {
             for (int w = 0; w <= W; w++) {
-                if (weights[i] <= w) {
-                    dp[i + 1][w] = Math.max(dp[i][w - weights[i]] + values[i], dp[i][w]);
+                // 品物iを選択できる(重量[i]がw以下)
+                if (weight[i] <= w) {
+                    // 選択できる場合、選択する方/しない方の大きい方を入力
+                    // (選択するには重量[i]分減らしたとこから遷移する必要がある)
+                    dp[i + 1][w] = Math.max(dp[i][w - weight[i]] + value[i], dp[i][w]);
                 } else {
+                    // 選択できない場合、それまでと値は変わらない
                     dp[i + 1][w] = dp[i][w];
                 }
             }
@@ -30,4 +36,4 @@ public class Main {
         
         System.out.println(dp[n][W]);
     }
-} 
+}
