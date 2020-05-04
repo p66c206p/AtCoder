9ffd4a6837e5eef2c6b341483fb90b99dcfea299
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-05-04 20:17:33 +0900

    数列をBFSで順に作る(x10して+num) ルンルン数(3234566667)

--- ABC161D.java
+++ ABC161D.java
@@ -6,42 +6,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
         
-        if (k <= 9) {
-            System.out.println(k);
-            return;
-        }
-        
+        // que: ルンルン数(次の桁に-1～+1で行ける)
+        // ex. 334, 2123, 3234566667
         Queue<Long> que = new ArrayDeque<Long>();
+        
+        // 1～9をキューに入れる
         for (int i = 1; i <= 9; i++) {
             que.add((long)i);
         }
         
-        int count = 9;
+        // count: キューから出した回数
+        int count = 0;
         while (true) {
             long cur = que.poll();
+            long x10 = cur * 10;
             long last = cur % 10;
+            count++;
             
-            if (last != 0) {
-                que.add(cur * 10 + last - 1);
-                count++;
-                if (k == count) {
-                    System.out.println(cur * 10 + last - 1);
-                    return;
-                }
+            // 終了条件: k番目の数が取り出された
+            if (count == k) {
+                System.out.println(cur);
+                return;
             }
-                que.add(cur * 10 + last);
-                count++;
-                if (k == count) {
-                    System.out.println(cur * 10 + last);
-                    return;
-                }
+            
+            // -1, 0, +1のパターンをキューに入れる
+            if (last != 0) {
+                que.add(x10 + last - 1);
+            }
+            // if (1 <= last <= 8)
+                que.add(x10 + last);
             if (last != 9) {
-                que.add(cur * 10 + last + 1);
-                count++;
-                if (k == count) {
-                    System.out.println(cur * 10 + last + 1);
-                    return;
-                }
+                que.add(x10 + last + 1);
             }
         }
     }
