8a7d587a105e445dfa7c878ad0395e46a46af900
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-05-09 16:31:19 +0900

    Zero-Sum Ranges 数列で、条件を満たすi, jのパターン数 (連続部分列の和が0になる)

--- AGC023A.java
+++ AGC023A.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Your code here!
+        // Your code here! 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] array = new int[n];
