49a9b625d4e63f8a55f3114c284ea3e6fd6193ef
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-05-16 12:13:56 +0900

    ナップサック問題 (品物Nまでなめた時の価値Vでの最小重量)

--- EDCE.java
+++ EDCE.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Main {
+    static long INF = 1001001001001001001L;
     public static void main(String[] args) throws Exception {
         // Your code here!
         Scanner sc = new Scanner(System.in);
@@ -15,32 +16,39 @@ public class Main {
             V += values[i];
         }
         
+        // dp[i][v]: 品物0~iまで考慮した上で、価値vでの最小重量
         long[][] dp = new long[n + 1][V + 1];
+        // 初期化
         for (int i = 0; i <= n; i++) {
             for (int v = 0; v <= V; v++) {
-                dp[i][v] = 100000000000l; // Vより大きく、weights[i]を足しても溢れない数
+                dp[i][v] = INF;
             }
         }
-        
         dp[0][0] = 0;
         
+        // 考慮する品物を1つずつ増やして考える
         for (int i = 0; i < n; i++) {
             for (int v = 0; v <= V; v++) {
-                if (values[i] <= v) {
+                // 品物iを選択できる(重量[i]がw以下)
+                if (v - values[i] >= 0) {
+                    // 選択できる場合、選択する方/しない方の最善を選ぶ
+                    // (選択するには重量[i]分少ないとこからなら遷移できる)
                     dp[i + 1][v] = Math.min(dp[i][v - values[i]] + weights[i], dp[i][v]);
                 } else {
+                    // 選択できない場合、品物i-1の時と変わらない
                     dp[i + 1][v] = dp[i][v];
                 }
             }
         }
         
-        int answer = 0;
-        for (int v = 0; v <= V; v++) {
+        // ans: 品物nまでなめた時の、重量がW以下で最大の価値
+        int ans = 0;
+        for (int v = V; v >= 0; v--) {
             if (dp[n][v] <= W) {
-                answer = v;
+                ans = v;
+                System.out.println(ans);
+                return;
             }
         }
-        
-        System.out.println(answer);
     }
 }
