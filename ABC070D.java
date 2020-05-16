0bf380fb94605d286d45da9cfcfaa566f2c15b40
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-05-16 11:27:26 +0900

    木 最短経路問題(DFS) 点Kから各点までの最短距離全て(O(N-1))(木なので)

--- ABC070D.java
+++ ABC070D.java
@@ -23,36 +23,28 @@ public class Main {
             to[q].add(new int[]{p, dis});
         }
         
-        // dist: kから各点への最短距離
+        // dist[i]: kからiへの最短距離
         int query = sc.nextInt();
         int k = sc.nextInt() - 1;
-        long[] distK = calcDist(k);
+        dist = new long[n];
+        dfs(k, 0, -1);
         
         // p→k→qの最短距離を出力
         while (query-- > 0) {
             int p = sc.nextInt() - 1;
             int q = sc.nextInt() - 1;
             
-            System.out.println(distK[p] + distK[q]);
+            System.out.println(dist[p] + dist[q]);
         }
     }
     
-    public static long[] calcDist(int p) {
-        dist = new long[n];
-        dfs(p, 0, -1);
-        return dist;
-    }
-    
-    // 子qに、「pまでの距離+qへの距離」を配る
+    // 頂点pと隣接する点に自分の値+距離を配る
     public static void dfs(int p, long d, int par) {
         dist[p] = d;
         
-        for (int[] qData : to[p]) {
-            int q = qData[0];
-            int PtoQ = qData[1];
-            if (q == par) continue;
-            
-            dfs(q, d + PtoQ, p);
+        for (int[] q : to[p]) {
+            if (q[0] == par) continue;
+            dfs(q[0], d + q[1], p);
         }
     }
 }
