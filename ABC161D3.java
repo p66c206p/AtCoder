e2c7bdc60ec56c29f3f8d986dab4e8f24fa4ba13
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-05-19 22:14:32 +0900

    枝刈り全探索 (forループ(10^9))

--- ABC161D3.java
+++ ABC161D3.java
@@ -35,7 +35,7 @@ public class Main {
                     }
                     
                     i += tmp;
-                    --;
+                    i--;
                     
                     break;
                 }
