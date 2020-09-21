32383bad51022bb95fd62f404f1ac5deea52c190
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-09-21 16:52:27 +0900

    周期(ρ法) (Teleporterの類題)

--- ABC179E.java
+++ ABC179E.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Your code here!
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
+        long k = sc.nextLong();
         int x = sc.nextInt();
         int m = sc.nextInt();
         
         // ans:
         // array = {2,4,<16,256,471,620>}について、
         // 「i=0からスタートし、">"まで来たら"<"に戻る」ルールで、
-        // 操作「今いるarray[i]の値を加算し、i++する」をn回したときの合計
+        // 操作「今いるarray[i]の値を加算し、i++する」をk回したときの合計
         
         // array: a[i+1] = (a[i] * a[i]) % m
         
@@ -20,10 +20,10 @@ public class Main {
         Set<Integer> set = new HashSet<Integer>();
         List<Integer> list = new ArrayList<Integer>();
         int now = x;
-        set.add(now;
+        set.add(now);
         list.add(now);
         int last = 0;
-        for (long i = 0; i < n; i++) {
+        for (long i = 0; i < k; i++) {
             now = (int)((long)now * now % m);
             if (set.contains(now)) {
                 last = now;
@@ -48,7 +48,7 @@ public class Main {
         for (int i = 0; i < loop_idx; i++) {
             ans += list.get(i);
         }
-        n -= loop_idx;
+        k -= loop_idx;
         
         // [loop始点, loop終点] * 何周できるか
         long loop_sum = 0;
@@ -56,12 +56,12 @@ public class Main {
             loop_sum += list.get(i);
         }
         long len = list.size() - loop_idx;
-        long block = n / len;
+        long block = k / len;
         ans += loop_sum * block;
-        n %= len;
+        k %= len;
         
         // [loop始点, 残りの移動回数]
-        for (int i = loop_idx; i < loop_idx + n; i++) {
+        for (int i = loop_idx; i < loop_idx + k; i++) {
             ans += list.get(i);
         }
         
