2deef9f82fe0aaf8137c25cd568b34981bf4c896
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-09-22 16:44:55 +0900

    セグ木(Range Max Query)

---
+++ ABC047D.java
@@ -0,0 +1,95 @@
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        SegmentTree<Integer> seg = new SegmentTree<>(n, Integer::max, Integer.MIN_VALUE);
+        
+        // 初期の配列を取得
+        for (int i = 0; i < n; i++) {
+            seg.update(i, sc.nextInt());
+        }
+        
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            int now = seg.query(i, i+1);
+            int max = seg.query(i+1, n);
+            
+            if (now < max) {
+                int key = max - now;
+                int val = map.getOrDefault(key, 0);
+                map.put(key, ++val);
+            }
+        }
+        
+        int last_val = map.get(map.lastKey());
+        System.out.println(last_val);
+    }
+}
+
+class SegmentTree<T> {
+    // dat: 0-indexed
+    // 00000000
+    // 11112222
+    // 33445566
+    // 789ABCDE ... a[0]-a[n]
+    
+    final T[] dat;              // データを格納する配列
+    final int n;                // datの一番下のレベルの要素数
+    final BinaryOperator<T> f;  // 二項演算子
+    final T e;                  // 単位元
+
+    // 単位元で埋めて新規作成
+    public SegmentTree(int len, BinaryOperator<T> f, T e) {
+        int n = 1;
+        while (n < len) n <<= 1;
+        this.n = n;
+        this.f = f;
+        this.e = e;
+        this.dat = (T[]) new Object[n << 1];
+        Arrays.fill(dat, e);
+    }
+    
+    // 一点更新(a[i] = x) O(logN)
+    public void update(int i, T x) {
+        i += n - 1;
+        dat[i] = x;
+        while (i > 0) {
+            i = (i - 1) / 2;  // i: parent of vl/vr
+            T vl = dat[i * 2 + 1];
+            T vr = dat[i * 2 + 2];
+            dat[i] = f.apply(vl, vr);
+        }
+    }
+    
+    // 区間取得[a, b) O(logN)
+    public T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
+    public T query_sub(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l) {             // outside completely
+            return e;
+        } else if (a <= l && r <= b) {      // inside completely
+            return dat[k];
+        } else {
+            T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
+            T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+            return f.apply(vl, vr);
+        }
+    }
+    
+    // 使用例
+    // Range Sum Query
+    // SegmentTree<Integer> seg = new SegmentTree<>(n, Integer::sum, 0);
+    // SegmentTree<Long> seg = new SegmentTree<>(n, Long::sum, 0L);
+    
+    // Range Minimum Query
+    // SegmentTree<Integer> seg = new SegmentTree<>(n, Integer::min, Integer.MAX_VALUE);
+    
+    // Range Maximum Query
+    // SegmentTree<Long> seg = new SegmentTree<>(n, Long::max, Long.MIN_VALUE);
+}
