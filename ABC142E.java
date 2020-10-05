161a35720667ed104e1a9b411df443a2a84f27a9
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-10-05 21:52:27 +0900

    bitDP (集合jへの最小コスト) (宝箱の鍵)

--- ABC142E.java
+++ ABC142E.java
@@ -1,48 +1,65 @@
 import java.util.*;
 
 public class Main {
-    static long INF = 1001001001001001001L;
+    static long INF = (long)1e+18;
+    
     public static void main(String[] args) throws Exception {
         // Your code here!
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] array = new int[m][2];
+        int all = (1<<n) - 1;
         
         for (int i = 0; i < m; i++) {
             int cost = sc.nextInt();
-            int k = sc.nextInt();
+            int kind = sc.nextInt();
             
-            // 集合s (ex. 1011 = bit0,1,3を含む集合)
+            // 集合s (bitで持つ)
+            // ex. {0,2,3} -> 0b1101 (0-indexed)
             int s = 0;
-            for (int j = 0; j < k; j++) {
+            for (int j = 0; j < kind; j++) {
                 int digit = sc.nextInt() - 1;
                 
-                // digit番目のbitを立てる
+                // digit桁目のbitを立てる
                 s |= 1<<digit;
             }
             
-            array[i][0] = s;
-            array[i][1] = cost;
+            array[i][0] = cost;
+            array[i][1] = s;
         }
         
-        // dp[s] = 集合sに至るまでの最小コスト
-        long[] dp = new long[1<<n];
-        Arrays.fill(dp, INF);
-        dp[0] = 0;
-        for (int s = 0; s < 1<<n; s++) {
-            for (int i = 0; i < m; i++) {
-                // t = 集合sからi番目の鍵を使って行ける集合
-                int t = s | array[i][0];
-                int cost = array[i][1];
-                
-                // 最小コストの更新
-                dp[t] = Math.min(dp[t], dp[s] + cost);
+        // dp[i][j]: アイテムiまで見てきたときの、集合jの最小コスト
+        long[][] dp = new long[m+1][all+1];
+        // 初期化
+        for (long[] dps : dp) {
+            Arrays.fill(dps, INF);
+        }
+        dp[0][0] = 0;
+        
+        for (int i = 0; i < m; i++) {
+            int c = array[i][0];
+            int s = array[i][1];
+            for (int j = 0; j <= all; j++) {
+                for (int k = 0; k <= 1; k++) {
+                    // 状態jでアイテムiをk個使う
+                    int ni = i+1;
+                    int nj = j | s * k;
+                    long plus = c * k;
+                    
+                    // カットとかcontinue
+                    
+                    // 遷移
+                    dp[ni][nj] = Math.min(dp[ni][nj], dp[i][j] + plus);
+                }
             }
         }
+        // for (long[] dps : dp) {
+        //     System.out.println(Arrays.toString(dps));
+        // }
         
-        // 演算順位に注意
-        if (dp[(1<<n) - 1] == INF) dp[(1<<n) - 1] = -1;
-        System.out.println(dp[(1<<n) - 1]);
+        long ans = dp[m][all];
+        if (ans == INF) ans = -1;
+        System.out.println(ans);
     }
 }
