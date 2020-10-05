7628b6b3b24adadf4b2b19833ef2f8e97da67297
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-10-05 23:06:01 +0900

    ナップサックDP(個数制限なし) (ダメージjの最小MP)

--- ABC153E.java
+++ ABC153E.java
@@ -1,52 +1,69 @@
 import java.util.*;
 
 public class Main {
-    static int INF = 1001001009;
+    static long INF = (long)1e+18;
     
     public static void main(String[] args) throws Exception {
         // Your code here!
         Scanner sc = new Scanner(System.in);
-        int W = sc.nextInt();
+        int H = sc.nextInt();
         int n = sc.nextInt();
-        int[] weights = new int[n];
-        int[] values = new int[n];
+        int V = H;
+        
+        int[][] array = new int[n][2];
         for (int i = 0; i < n; i++) {
-            weights[i] = sc.nextInt();
-            values[i] = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            array[i][0] = v;
+            array[i][1] = w;
         }
         
-        // dp[i][w]: 品物0~iまでなめた時のwダメージ与えられる最小コスト
-        int[][] dp = new int[n + 1][W + 1];
+        // dp[i][j]: アイテムiまで見てきたときの、ダメージjの最小MP
+        long[][] dp = new long[n+1][V+1];
         // 初期化
+        for (long[] dps : dp) {
+            Arrays.fill(dps, INF);
+        }
+        dp[0][0] = 0;
+        
+        // 「アイテムiを1回使うと状態jの区間[0, v]はwになる」を先にしておく
+        // (下のdpを配るDPでやってるから)
         for (int i = 0; i < n; i++) {
-            for (int w = 0; w <= W; w++) {
-                dp[i][w] = INF;
+            int v = array[i][0];
+            int w = array[i][1];
+            for (int j = 0; j <= Math.min(v, V); j++) {
+                dp[i+1][j] = w;
             }
         }
-        // 0ダメージ与えられる最小のコストは0
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 0;
-        }
         
-        // dp[i + 1][w] (品物iまでを使ったときの)wダメージ与えられる最小のコスト
         for (int i = 0; i < n; i++) {
-            for (int w = 0; w <= W; w++) {
-                // 品物iを選択できる(重量[i]がw以下)
-                if (w - weights[i] >= 0) {
-                    // 選択できる場合、選択する方/しない方の最善を選ぶ
-                    // (選択するには品物"i"のダメージ[i]分少ないとこからなら遷移できる)
-                    // (※個数制限なしなので自分(i)から遷移できる (ありならi-1から遷移))
-                    dp[i + 1][w] = Math.min(dp[i + 1][w - weights[i]] + values[i], dp[i][w]);
-                } else {
-                    // 選択できない場合、ダメージ0から遷移する
-                    dp[i + 1][w] = Math.min(dp[i + 1][0] + values[i], dp[i][w]);
+            int v = array[i][0];
+            int w = array[i][1];
+            for (int j = 0; j <= V; j++) {
+                for (int k = 0; k <= 1; k++) {
+                    // 状態jでアイテムiをk個使う
+                    int ni = i+1;
+                    int nj = j + v * k;
+                    long plus = w * k;
+                    
+                    // カットとかcontinue
+                    if (nj > V) continue;
+                    
+                    // 遷移:
+                    // ni行の0~j列まではbestが確定しているので、
+                    // もう1個アイテムを使うときの最善元は(ni,j)
+                    if (k == 0) {
+                        dp[ni][nj] = Math.min(dp[ni][nj], dp[i][j] + plus);
+                    } else {
+                        dp[ni][nj] = Math.min(dp[ni][nj], dp[ni][j] + plus);
+                    }
                 }
             }
         }
-        // for (int i = 0; i < n+1; i++) {
-        //     System.out.println(Arrays.toString(dp[i]));
+        // for (long[] dps : dp) {
+        //     System.out.println(Arrays.toString(dps));
         // }
         
-        System.out.println(dp[n][W]);
+        System.out.println(dp[n][V]);
     }
 }
