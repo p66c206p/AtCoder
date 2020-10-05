374eb9c41d2ebc91bf7aa7b10b7a59cb95bbccb6
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-10-05 21:10:59 +0900

    ナップサックDP (重さjの最大価値)

--- EDCD.java
+++ EDCD.java
@@ -6,39 +6,44 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int W = sc.nextInt();
-        int[] weight = new int[n];
-        int[] value = new int[n];
+        
+        int[][] array = new int[n][2];
         for (int i = 0; i < n; i++) {
-            weight[i] = sc.nextInt();
-            value[i] = sc.nextInt();
+            int w = sc.nextInt();
+            int v = sc.nextInt();
+            array[i][0] = w;
+            array[i][1] = v;
         }
         
-        // dp[i][w]: 品物0~iまで考慮した上で、重量wでの最高価値
-        int[][] dp = new int[n + 1][W + 1];
+        // dp[i][j]: アイテムiまで見てきたときの、重さjの最大価値
+        long[][] dp = new long[n+1][W+1];
         // 初期化
-        for (int w = 0; w <= W; w++) {
-            dp[0][w] = 0;
+        for (int j = 0; j <= W; j++) {
+            dp[0][j] = 0;
         }
         
-        // 考慮する品物を1つずつ増やして考える
         for (int i = 0; i < n; i++) {
-            for (int w = 0; w <= W; w++) {
-                // 品物iを選択できる(重量[i]がw以下)
-                if (w - weight[i] >= 0) {
-                    // 選択できる場合、選択する方/しない方の最善を選ぶ
-                    // (選択するには品物i-1の重量[i]分少ないとこからなら遷移できる)
-                    dp[i + 1][w] = Math.max(dp[i][w - weight[i]] + value[i], dp[i][w]);
-                } else {
-                    // 選択できない場合、品物i-1の時と変わらない
-                    dp[i + 1][w] = dp[i][w];
+            int w = array[i][0];
+            int v = array[i][1];
+            for (int j = 0; j <= W; j++) {
+                for (int k = 0; k <= 1; k++) {
+                    // 状態jでアイテムiをk個使う
+                    int ni = i+1;
+                    int nj = j + w * k;
+                    long plus = v * k;
+                    
+                    // カットとかcontinue
+                    if (nj > W) continue;
+                    
+                    // 遷移
+                    dp[ni][nj] = Math.max(dp[ni][nj], dp[i][j] + plus);
                 }
             }
         }
-        // for (int i = 0; i < n+1; i++) {
-        //     System.out.println(Arrays.toString(dp[i]));
+        // for (long[] dps : dp) {
+        //     System.out.println(Arrays.toString(dps));
         // }
         
-        // ans: 品物nまでなめた時の、重量Wの最大価値
         System.out.println(dp[n][W]);
     }
 }
