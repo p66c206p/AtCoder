c533ec53fdd53baaaf86e3b377bf5d55d5d3ef06
p66c206p <42195846+p66c206p@users.noreply.github.com>
2020-12-22 00:35:10 +0900

    逆元 a^(-1) (mod m) modinv

--- 0inv_mod.java
+++ 0inv_mod.java
@@ -1,17 +1,17 @@
     // 逆元 O(log m) (m = mod)
     // ex. 2 -> 500000004
     public static long inv_mod(long a, long mod) {
-		long b = mod;
-		long p = 1, q = 0;
-		while (b > 0) {
-			long c = a / b;
-			long d;
-			d = a;
-			a = b;
-			b = d % b;
-			d = p;
-			p = q;
-			q = d - c * q;
-		}
-		return p < 0 ? p + mod : p;
-	}
+        long b = mod;
+        long p = 1, q = 0;
+        while (b > 0) {
+            long c = a / b;
+            long d;
+            d = a;
+            a = b;
+            b = d % b;
+            d = p;
+            p = q;
+            q = d - c * q;
+        }
+        return p < 0 ? p + mod : p;
+    }
