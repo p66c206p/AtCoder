8f8aa9af67052e13a4e5e0797e05f0de24035d27
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-01-28 22:43:23 +0900

    木 BFS(根から順に親から子へ色を配る) 隣接リスト

--- ABC146D.java
+++ ABC146D.java
@@ -6,40 +6,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        // index[i]: i番目に出力する点
-        int[] index = new int[n-1];
-        
-        // to: 隣接リスト
-        List<Integer>[] to = new List[n];
-        for (int i = 0; i < n; i++) {
-            to[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < n - 1; i++) {
-            int s = sc.nextInt() - 1;
-            int t = sc.nextInt() - 1;
-            to[s].add(t);
-            index[i] = t;
-        }
-        
-        // visited: 当該点を既に通ったか否か
-        boolean[] visited = new boolean[n];
-        visited[0] = true;
-        
-        
-        // max: colorsの最大値
-        // colors[i]: iの色
-        int max = 0;
-        int[] colors = new int[n];
-        // 頂点0から順に、親から子へ初期値を配り最終値を求める
-        Queue<int[]> que = new ArrayDeque<int[]>();
-        que.add(new int[]{0, 0});import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        // Your code here! 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
         // to: 隣接リスト(無向)
         List<Integer>[] to = new List[n];
         Map<String, Integer> edge_idxes = new HashMap<>();
@@ -88,28 +54,6 @@ public class Main {
         System.out.println(count);
         for (int color : colors) {
             System.out.println(color);
-        }
-    }
-}
-        while (!que.isEmpty()) {
-            int[] cur = que.poll();
-            int p = cur[0];
-            int color = cur[1];
-            
-            int now = 1;
-            for (Integer q : to[p]) {
-                if (now == color) now++;
-                que.add(new int[]{q, now});
-                colors[q] = now;
-                max = Math.max(max, now);
-                now++;
-            }
-        }
-        
-        // colors[x]をindex[i]順に出力
-        System.out.println(max);
-        for (int i = 0; i < n-1; i++) {
-            System.out.println(colors[index[i]]);
         }
     }
 }
