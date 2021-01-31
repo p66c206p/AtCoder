92430b1835f22a2fd089c8c31ad66608572855a3
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-01-31 15:21:12 +0900

    DFS (木 最短経路問題) (2頂点からDFS、木で鬼ごっこ)

--- ABC148F.java
+++ ABC148F.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class Main {
@@ -12,12 +13,11 @@ public class Main {
         int u = sc.nextInt() - 1;
         int v = sc.nextInt() - 1;
         
+        // to: 隣接リスト(無向)
         to = new List[n];
         for (int i = 0; i < n; i++) {
-            to[i] = new ArrayList<Integer>();
+            to[i] = new ArrayList<>();
         }
-        
-        // to: 隣接リスト(無向)
         for (int i = 0; i < n - 1; i++) {
             int s = sc.nextInt() - 1;
             int t = sc.nextInt() - 1;
@@ -25,14 +25,26 @@ public class Main {
             to[t].add(s); 
         }
         
-        // u/vから各点までの距離を計算
+        // ans:
+        // U地点とV地点から鬼ごっこをする。（先手はUにいる人、後手が鬼）
+        // 2人が交わったら終了。終了した時、後手の歩数の最小値は？
+        
+        // how:
+        // 先手は自分のほうが早く行ける点のうち、
+        // 最も遠い点に行くのが最適。
+        // 後手の歩数の最小値は、distV[その点] - 1。
+        // (交わるのは必ず、その点の一歩手前なので)
+        
+        // distU: 頂点Uからの距離
+        // distV: 頂点Vからの距離
         int[] distU = calcDist(u);
         int[] distV = calcDist(v);
         
-        // uの方が近い点のうち、最も遠い点までの距離を取得
+        // Uの方が近い点のうち、最も遠い点までの距離を取得
         int max = -1;
         for (int i = 0; i < n; i++) {
             if (distU[i] < distV[i]) {
+                System.out.println(i + " " + distU[i] + " " + distV[i]);
                 max = Math.max(max, distV[i]);
             }
         }
@@ -40,13 +52,14 @@ public class Main {
         System.out.println(max - 1);
     }
     
+    // calcDist: 頂点pからDFSする
     public static int[] calcDist(int p) {
         dist = new int[n];
         dfs(p, 0, -1);
         return dist;
     }
     
-    // 子qに、距離+1を配る
+    // DFS: ある頂点からの最短歩数を求める
     public static void dfs(int p, int d, int par) {
         dist[p] = d;
         
