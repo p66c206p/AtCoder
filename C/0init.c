383f390f4973a0d9d56b0a7a147dd202596875a0
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-03 20:56:07 +0900

    Update 0init.c

--- C/0init.c
+++ C/0init.c
@@ -3,6 +3,8 @@ using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define ll long long
+#define vi vector<int>
+#define sort(v) sort(v.begin(), v.end())
 
 int main(void){
     
