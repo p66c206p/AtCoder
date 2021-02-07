87eab7c3a7b887d06ca555392a318718b3d55f67
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-07 13:05:04 +0900

    mapでダイクストラ法 (点N→点0への最短距離 (N <= 10^18)) Pay to Win

--- AGC044A.java
+++ AGC044A.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
     static long INF = 1001001001001001018L;
+    static long n, a, b, c, d;
     
     public static void main(String[] args) throws Exception {
         // Your code here!
@@ -17,142 +18,149 @@ public class Main {
         // 5. n -> n+1 (d)
         
         while (t--> 0) {
-            long n = sc.nextLong();
-            long a = sc.nextLong();
-            long b = sc.nextLong();
-            long c = sc.nextLong();
-            long d = sc.nextLong();
+            n = sc.nextLong();
+            a = sc.nextLong();
+            b = sc.nextLong();
+            c = sc.nextLong();
+            d = sc.nextLong();
             
             long start = n;
             long goal = 1;
-            
-            // dist: startからの最短距離
+        
+            // ダイクストラ法 O((N+M)logM)
+            // 使用条件: (辺の重さバラバラ, 辺の重さが負でない)
             // ！配列ではなくmapで持つ。
             // <- 状態数が10^18もあるから。
             // <- (また、間引きできるので全状態を構える必要がないから。)
-            Map<Long, Long> dist = new HashMap<Long, Long>();
-            dist.put(start, 0L);
+            Map<Long, Long> dist = dijkstra(start, goal);
             
-            // ダイクストラ法: BFSで距離を配る。
-            // (キューの先頭の点はそう来る他最短経路がないので最短距離が確定する)
-            PriorityQueue<Point> que = new PriorityQueue<>();
-            que.add(new Point(start, 0));
-            while (!que.isEmpty()) {
-                Point cur = que.poll();
-                long p = cur.name;
-                long dis = cur.dist;
-                
-                if (p == goal) {
-                    System.out.println(dis + d);
-                    break;
+            // ans: 頂点N->頂点1 + 頂点1->頂点0
+            long ans = dist.get(goal) + d;
+            System.out.println(ans);
+        }
+    }
+    
+    public static Map<Long, Long> dijkstra(long start, long goal) {
+        // dist: startからの最短距離
+        Map<Long, Long> dist = new HashMap<>();
+        dist.put(start, 0L);
+        
+        // ダイクストラ法: BFSで距離を配る。
+        // (キューの先頭の点はそう来る他最短経路がないので最短距離が確定する)
+        PriorityQueue<Point> que = new PriorityQueue<>();
+        que.add(new Point(start, 0L));
+        while (!que.isEmpty()) {
+            Point cur = que.poll();
+            long p = cur.name;
+            long dis = cur.dist;
+            
+            // distより大きいdisを持つ点からは配らせない
+            if (dis > dist.getOrDefault(p, INF)) continue;
+            
+            if (p <= 1) continue;
+            
+            // // 確定したpとdis
+            // System.out.println(p + " " + dis);
+            
+            // 子に「自分の距離 + p→qの距離」を配る
+            // 最短距離が更新される場合だけキューに入れる
+            long q;
+            long PtoQ;
+            
+            // 遷移パターン: ex. 11 -> q (移動コスト)
+            // (中間点はキューに入れない)
+            // 1. 1まで-1ずつ移動
+            // -> 11->1                     ((p-1)*d)
+            // 2. 2の倍数(上下)に移動し、/2の点へ移動
+            // -> 11(->10)->5, 11(->12)->6  (2の倍数までの距離*d + a)
+            // 3. 3の倍数(上下)に移動し、/3の点へ移動
+            // -> 11(->9)->3, 11(->12)->4   (3の倍数までの距離*d + b)
+            // 4. 5の倍数(上下)に移動し、/5の点へ移動
+            // -> 11(->10)->2, 11(->15)->3  (5の倍数までの距離*d + c)
+            
+            // ->goalのパターン
+            q = 1;
+            PtoQ = (p-1) * d;
+            // if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+            if (dis / d + (p-1) < dist.getOrDefault(q, INF) / d) {  // オーバーフロー対策
+                dist.put(q, dis + PtoQ);
+                que.add(new Point(q, dist.get(q)));
+            }
+            
+            // /2のパターン
+            if (p % 2 == 0) {
+                q = p / 2;
+                PtoQ = a;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+                    dist.put(q, dis + PtoQ);
+                    que.add(new Point(q, dist.get(q)));
                 }
-                
-                // distより大きいdisを持つ点からは配らせない
-                if (dis > dist.getOrDefault(p, INF)) continue;
-                
-                if (p <= 1) continue;
-                
-                // // 確定したpとdis
-                // System.out.println(p + " " + dis);
-                
-                // 子に「自分の距離 + p→qの距離」を配る
-                // 最短距離が更新される場合だけキューに入れる
-                long q;
-                long PtoQ;
-                
-                // 遷移パターン: ex. 11 -> q (移動コスト)
-                // (中間点はキューに入れない)
-                // 1. 1まで-1ずつ移動
-                // -> 11->1                     ((p-1)*d)
-                // 2. 2の倍数(上下)に移動し、/2の点へ移動
-                // -> 11(->10)->5, 11(->12)->6  (2の倍数までの距離*d + a)
-                // 3. 3の倍数(上下)に移動し、/3の点へ移動
-                // -> 11(->9)->3, 11(->12)->4   (3の倍数までの距離*d + b)
-                // 4. 5の倍数(上下)に移動し、/5の点へ移動
-                // -> 11(->10)->2, 11(->15)->3  (5の倍数までの距離*d + c)
-                
-                // ->goalのパターン
-                q = 1;
-                PtoQ = (p-1) * d;
-                // if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                if (dis / d + (p-1) < dist.getOrDefault(q, INF) / d) {  // オーバーフロー対策
+            } else {
+                q = p / 2;
+                PtoQ = d * (p % 2) + a;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
                     dist.put(q, dis + PtoQ);
                     que.add(new Point(q, dist.get(q)));
                 }
                 
-                // /2のパターン
-                if (p % 2 == 0) {
-                    q = p / 2;
-                    PtoQ = a;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
-                } else {
-                    q = p / 2;
-                    PtoQ = d * (p % 2) + a;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
-                    
-                    q = (p+1) / 2;
-                    PtoQ = d * (2 - (p % 2)) + a;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
+                q = (p+1) / 2;
+                PtoQ = d * (2 - (p % 2)) + a;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+                    dist.put(q, dis + PtoQ);
+                    que.add(new Point(q, dist.get(q)));
+                }
+            }
+            
+            // /3のパターン
+            if (p % 3 == 0) {
+                q = p / 3;
+                PtoQ = b;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+                    dist.put(q, dis + PtoQ);
+                    que.add(new Point(q, dist.get(q)));
+                }
+            } else {
+                q = p / 3;
+                PtoQ = d * (p % 3) + b;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+                    dist.put(q, dis + PtoQ);
+                    que.add(new Point(q, dist.get(q)));
                 }
                 
-                // /3のパターン
-                if (p % 3 == 0) {
-                    q = p / 3;
-                    PtoQ = b;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
-                } else {
-                    q = p / 3;
-                    PtoQ = d * (p % 3) + b;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
-                    
-                    q = (p+2) / 3;
-                    PtoQ = d * (3 - (p % 3)) + b;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
+                q = (p+2) / 3;
+                PtoQ = d * (3 - (p % 3)) + b;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+                    dist.put(q, dis + PtoQ);
+                    que.add(new Point(q, dist.get(q)));
+                }
+            }
+            
+            // /5のパターン
+            if (p % 5 == 0) {
+                q = p / 5;
+                PtoQ = c;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+                    dist.put(q, dis + PtoQ);
+                    que.add(new Point(q, dist.get(q)));
+                }
+            } else {
+                q = p / 5;
+                PtoQ = d * (p % 5) + c;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+                    dist.put(q, dis + PtoQ);
+                    que.add(new Point(q, dist.get(q)));
                 }
                 
-                // /5のパターン
-                if (p % 5 == 0) {
-                    q = p / 5;
-                    PtoQ = c;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
-                } else {
-                    q = p / 5;
-                    PtoQ = d * (p % 5) + c;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
-                    
-                    q = (p+4) / 5;
-                    PtoQ = d * (5 - (p % 5)) + c;
-                    if (dis + PtoQ < dist.getOrDefault(q, INF)) {
-                        dist.put(q, dis + PtoQ);
-                        que.add(new Point(q, dist.get(q)));
-                    }
+                q = (p+4) / 5;
+                PtoQ = d * (5 - (p % 5)) + c;
+                if (dis + PtoQ < dist.getOrDefault(q, INF)) {
+                    dist.put(q, dis + PtoQ);
+                    que.add(new Point(q, dist.get(q)));
                 }
             }
         }
+        
+        return dist;
     }
 }
     
