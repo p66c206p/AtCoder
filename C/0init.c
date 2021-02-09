b9b79022529d8281dddd38cb98f846fc07c92b2b
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-09 11:16:09 +0900

    Update 0init.c

--- C/0init.c
+++ C/0init.c
@@ -4,10 +4,12 @@ using namespace std;
 typedef long long ll;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define print(x) cout << x << endl
+#define print(x) cout << (x) << endl
 #define vi vector<int>
 #define vvi vector<vector<int>>
 #define all(v) v.begin(),v.end()
+#define YN(ok) print(ok ? "YES" : "NO")
+#define yn(ok) print(ok ? "YES" : "NO")
 
 int n, k;
 string s;
