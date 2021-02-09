36788282bb593c4ca79f1e192d32d7428b73aa2f
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-09 15:00:30 +0900

    Update 0init.c

--- C/0init.c
+++ C/0init.c
@@ -4,16 +4,32 @@ using namespace std;
 typedef long long ll;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define print(x) cout << (x) << endl
+#define print(x) cout << x << endl
 #define vi vector<int>
 #define vvi vector<vi>
 #define all(v) (v).begin(),(v).end()
 #define YN(ok) print(ok ? "YES" : "NO")
 #define yn(ok) print(ok ? "YES" : "NO")
+#define decimal(n) setprecision(12) << n
 
 int n, k;
 string s;
 
 int main(void){
+    cin >> n >> k;
     
+    double res = 0;
+    rep(i, n+1) {
+        if (i == 0) continue;
+        
+        double tmp = (double)1 / n;
+        int now = i;
+        while (now < k) {
+            now *= 2;
+            tmp /= 2;
+        }
+        
+        res += tmp;
+    }
+    print(decimal(res));
 }
