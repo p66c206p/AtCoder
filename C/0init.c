e617f143a62a0d2e5912113740ad7461091975ed
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-09 16:25:12 +0900

    Update 0init.c

--- C/0init.c
+++ C/0init.c
@@ -9,27 +9,12 @@ typedef long long ll;
 #define vvi vector<vi>
 #define all(v) (v).begin(),(v).end()
 #define YN(ok) print(ok ? "YES" : "NO")
-#define yn(ok) print(ok ? "YES" : "NO")
+#define yn(ok) print(ok ? "Yes" : "No")
 #define decimal(n) setprecision(12) << n
 
 int n, k;
 string s;
 
 int main(void){
-    cin >> n >> k;
     
-    double res = 0;
-    rep(i, n+1) {
-        if (i == 0) continue;
-        
-        double tmp = (double)1 / n;
-        int now = i;
-        while (now < k) {
-            now *= 2;
-            tmp /= 2;
-        }
-        
-        res += tmp;
-    }
-    print(decimal(res));
 }
