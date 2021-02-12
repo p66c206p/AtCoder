cf657710e2ddb3bf30658fe9c6f2b4c7df36f4d5
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-12 16:07:04 +0900

    Update 000init.cpp

--- 000init.cpp
+++ 000init.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> P;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define print(x) cout << x << endl
-#define vi vector<int>
-#define vvi vector<vi>
 #define all(v) (v).begin(),(v).end()
 #define YN(ok) print((ok ? "YES" : "NO"))
 #define yn(ok) print((ok ? "Yes" : "No"))
 #define decimal(n) setprecision(12) << n
 
 #define Arrays_toString(v) rep(o,(v).size()){cout<<v[o]<<", ";if(o==(v).size()-1){cout<<endl;}}
+template<class T> void que_toString(queue<T> q){while(q.size()){cout<<q.front()<<", ";q.pop();if(q.empty()){cout<<endl;}}}
 
 int n, k;
 string s;
