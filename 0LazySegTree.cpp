20cdbea480b2b3abba186b21acd627aae6757d0f
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-19 13:23:06 +0900

    遅延セグ木 (区間更新と区間取得がO(logN)でできる)

--- 0LazySegTree.cpp
+++ 0LazySegTree.cpp
@@ -4,28 +4,28 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef pair<int, int> P;
- 
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define print(x) cout << x << endl
 #define all(v) (v).begin(),(v).end()
 #define YN(ok) print((ok ? "YES" : "NO"))
 #define yn(ok) print((ok ? "Yes" : "No"))
 #define decimal(n) setprecision(12) << n
- 
+
 #define Arrays_toString(v) rep(o,(v).size()){cout<<v[o]<<", ";if(o==(v).size()-1){cout<<endl;}}
 template<class T> void que_toString(queue<T> q){while(q.size()){cout<<q.front()<<", ";q.pop();if(q.empty()){cout<<endl;}}}
- 
+
 #include <atcoder/all>
 using namespace atcoder;
 // using mint = modint998244353;
 // using mint = modint1000000007;
- 
+
 // dat
 struct S{
     ll val;
     int size;
 };
- 
+
 // lazy
 // struct F{
 //     mint b;
@@ -33,35 +33,35 @@ struct S{
 // };
 using F = ll;
 const F ID = 0;
- 
+
 // 二項演算子(dat vs dat)
 S op(S l, S r){
     return S{l.val + r.val, l.size + r.size};
 }
- 
+
 // 単位元(dat)
 S e(){
     return S{0, 0};
 }
- 
+
 // 二項演算子(lazy vs dat)
 S mapping(F f, S s){
     if (f == ID) return s;
     return S{(f * s.size),  s.size};
 }
- 
+
 // 二項演算子(lazy vs lazy)
 // 引数の順番に注意(m1,m2の順に処理)
 F composition(F m2, F m1){
     if (m2 == ID) return m1;
     return m2;
 }
- 
+
 // 単位元(lazy)
 F id(){
     return ID;
 }
- 
+
 int main(void){
     int n, q;
     cin >> n >> q;
