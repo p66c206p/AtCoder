21d89f26fd4454192de9783b00766b5949b3abdb
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-19 13:21:44 +0900

    遅延セグ木 (操作: 加算, クエリ: 区間max)

---
+++ code-festival-2015-finalD.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> P;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define print(x) cout << x << endl
+#define all(v) (v).begin(),(v).end()
+#define YN(ok) print((ok ? "YES" : "NO"))
+#define yn(ok) print((ok ? "Yes" : "No"))
+#define decimal(n) setprecision(12) << n
+
+#define Arrays_toString(v) rep(o,(v).size()){cout<<v[o]<<", ";if(o==(v).size()-1){cout<<endl;}}
+template<class T> void que_toString(queue<T> q){while(q.size()){cout<<q.front()<<", ";q.pop();if(q.empty()){cout<<endl;}}}
+
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+
+int INF = 1001001009;
+
+// dat
+// struct S{
+//     ll val;
+//     int size;
+// };
+using S = int;
+
+// lazy
+// struct F{
+//     mint b;
+//     mint c;
+// };
+using F = int;
+const F ID = 0;
+
+// 二項演算子(dat vs dat)
+S op(S l, S r){
+    return max(l, r);
+}
+
+// 単位元(dat)
+S e(){
+    return 0;
+}
+
+// 二項演算子(lazy vs dat)
+S mapping(F f, S s){
+    if (f == ID) return s;
+    return s+f;
+}
+
+// 二項演算子(lazy vs lazy)
+// 引数の順番に注意(m1,m2の順に処理)
+F composition(F m2, F m1){
+    if (m2 == ID) return m1;
+    return m1+m2;
+}
+
+// 単位元(lazy)
+F id(){
+    return ID;
+}
+
+int SIZE = 252521;
+
+int main(void){
+    int n, q;
+    cin >> n;
+    q = n;
+    
+    // 初期の配列を取得
+    vector<S> v(SIZE);
+    rep(i, SIZE) {
+        // int num; cin >> num;
+        v[i] = 0;
+    }
+	
+    lazy_segtree<S, op, e, F, mapping, composition, id> seg(v);
+    
+    vvi queries(n, vi(2));
+    rep(i, n)rep(j, 2) cin >> queries[i][j];
+    
+    for (auto query : queries) {
+        int l = query[0];
+        int r = query[1];
+        seg.apply(l, r, 1);
+    }
+    
+    int res = INF;
+    for (auto query : queries) {
+        int l = query[0];
+        int r = query[1];
+        
+        // remove
+        seg.apply(l, r, -1);
+        
+        res = min(res, seg.prod(0, SIZE));
+        
+        // return
+        seg.apply(l, r, 1);
+    }
+    print(res);
+}
