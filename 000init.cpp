37384c42e17b04f8e8002a28a5c26564de396ecc
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-02-25 12:26:04 +0900

    Update 000init.cpp

--- 000init.cpp
+++ 000init.cpp
@@ -13,7 +13,6 @@ typedef pair<int, int> P;
 #define decimal(n) setprecision(25) << n
 
 #define Arrays_toString(v) rep(o,(v).size()){cout<<v[o]<<", ";if(o==(v).size()-1){cout<<endl;}}
-template<class T> void que_toString(queue<T> q){while(q.size()){cout<<q.front()<<", ";q.pop();if(q.empty()){cout<<endl;}}}
 
 int main(void){
     
