feb0dad94ecdd08f6da9314262b78070bc75484e
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-03-18 22:47:32 +0900

    しゃくとり法(条件を満たす区間のうち最大/最小の長さ、または区間の個数を調べるのに有効)

--- 0shakutori.java
+++ 0shakutori.java
@@ -1,55 +1,55 @@
-public static int shakutori(int[] array, int k) {
-    // 条件を満たす区間の個数を返す
-    // この場合の条件: 任意の区間の和がk未満になる
-    
-    int count = 0;
-    int n = array.length;
-    int right = 0;
-    
-    int sum = 0;
-    
-    for (int left = 0; left < n; left++) {
-        while (right < n && sum + array[right] < k) {   // while (canGoNext)
-            sum += array[right];
-            right++;
+    public static long shakutori(int[] array, int k) {
+        // 条件を満たす区間の個数を返す
+        // この場合の条件: 任意の区間の和がk未満になる
+        
+        long count = 0;
+        int n = array.length;
+        int right = 0;
+        
+        int sum = 0;
+        
+        for (int left = 0; left < n; left++) {
+            while (right < n && sum + array[right] < k) {   // while (canGoNext)
+                sum += array[right];
+                right++;
+            }
+            
+            int countGot = right - left;
+            if (countGot > 0) {
+                count += countGot;
+                sum -= array[left];
+            } else {
+                right = left + 1;
+            }
         }
         
-        int countGot = right - left;
-        if (countGot > 0) {
-            count += countGot;
-            sum -= array[left];
-        } else {
-            right = left + 1;
-        }
+        return count;
     }
+
+    public static int shakutori(int[] array, int k) {
+        // 条件を満たす区間のうち最大の長さを返す
+        // この場合の条件: 区間の積がk以下になる
+        
+        int maxLength = 0;
+        int n = array.length;
+        int right = 0;
+        
+        long seki = 1;
+        
+        for (int left = 0; left < n; left++) {
+            while (right < n && seki * array[right] <= k) {     // while (canGoNext)
+                seki *= array[right];
+                right++;
+            }
+            
+            int countGot = right - left;
+            if (countGot > 0) {
+                maxLength = Math.max(maxLength, countGot);
+                seki /= array[left];
+            } else {
+                right = left + 1;
+            }
+        }
     
-    return count;
-}
-
-public static int shakutori(int[] array, int k) {
-    // 条件を満たす区間のうち最大の長さを返す
-    // この場合の条件: 区間の積がk以下になる
-
-    long maxLength = 0;
-    int n = array.length;
-    int right = 0;
-
-    long seki = 1;
-
-    for (int left = 0; left < n; left++) {
-        while (right < n && seki * array[right] <= k) {     // while (canGoNext)
-            seki *= array[right];
-            right++;
-        }
-
-        int countGot = right - left;
-        if (countGot > 0) {
-            maxLength = Math.max(maxLength, countGot);
-            seki /= array[left];
-        } else {
-            right = left + 1;
-        }
+        return maxLength;
     }
-
-    return maxLength;
-}
