132149d03a86d57ba8489fd2a82d435c08086c0d
p66c206p <42195846+p66c206p@users.noreply.github.com>
2021-06-26 16:38:26 +0900

    DP (配列から半分の要素を隣り合わないように選ぶ総和の最大値)

--- ABC162F.java
+++ ABC162F.java
@@ -2,59 +2,50 @@ import java.util.*;
 
 public class Main {
     static long INF = (long)1e+18;
+    
     public static void main(String[] args) throws Exception {
-        // Your code here! 
+        // Your code here!
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
         int[] array = new int[n];
         for (int i = 0; i < n; i++) {
             array[i] = sc.nextInt();
         }
         
-        // ans: 配列の中からn/2個選ぶ時の総和のmax
-        // (※ただし、隣り合う2要素は選べない)
+        // ans:
+        // 長さnの数列からどれも隣り合わないようにn/2個選ぶ総和のmax
         
-        // dp[i][j]: 最後にarray[i]を選択した時の、総和のmax
+        // how:
+        // n=6 -> 1個飛ばす、n=7 -> 2個飛ばす
+        // dpで(i番目, j個飛ばした)状態を持って遷移させていく。
+        
+        // dp[i][j]: (i番目, j個飛ばした)総和の最大値
         long[][] dp = new long[n+1][3];
         // 初期化
         for (long[] dps : dp) {
             Arrays.fill(dps, -INF);
         }
-        if (n % 2 == 0) {
-            dp[0][0] = 0;
-            dp[1][0] = array[0];
-            dp[2][1] = array[1];
-        } else {
-            dp[0][0] = 0;
-            dp[1][0] = array[0];
-            dp[2][1] = array[1];
-            dp[3][2] = array[2];
-        }
+        
+        dp[0][0] = 0;
+        dp[1][0] = array[0];
+        dp[2][1] = array[1];
+        if (n % 2 == 1) dp[3][2] = array[2];
         
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < 3; j++) {
-                if (j == 0) {
-                    if (i+2 <= n) {
-                        dp[i+2][j] = Math.max(dp[i+2][j], dp[i][j] + array[i+1]);
-                    }
-                    if (i+3 <= n) {
-                        dp[i+3][j+1] = Math.max(dp[i+3][j+1], dp[i][j] + array[i+2]);
-                    }
-                    if (i+4 <= n) {
-                        dp[i+4][j+2] = Math.max(dp[i+4][j+2], dp[i][j] + array[i+3]);
-                    }
-                } else if (j == 1) {
-                    if (i+2 <= n) {
-                        dp[i+2][j] = Math.max(dp[i+2][j], dp[i][j] + array[i+1]);
-                    }
-                    if (i+3 <= n) {
-                        dp[i+3][j+1] = Math.max(dp[i+3][j+1], dp[i][j] + array[i+2]);
-                    }
-                } else {
-                    if (i+2 <= n) {
-                        dp[i+2][j] = Math.max(dp[i+2][j], dp[i][j] + array[i+1]);
-                    }
+                for (int k = 0; j+k < 3; k++) {
+                    // ni: デフォルトで1個飛ばし(+2)
+                    // 追加でk個飛ばす
+                    int ni = i+2 + k;
+                    
+                    // nj: 飛ばした分だけ遷移
+                    int nj = j + k;
+                    
+                    // カットとかcontinue
+                    if (ni > n) continue;
+                    
+                    // 遷移
+                    dp[ni][nj] = Math.max(dp[ni][nj], dp[i][j] + array[ni-1]);
                 }
             }
         }
